d9a608d2d021f7643115b7235fe16e26108d0256
Mohamed Azmii <pydevazmi@gmail.com>
2023-07-21 21:22:01 +0300

    Time: 16 ms (89.60%), Space: 13.5 MB (49.26%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        re = bin(int(a,2) + int(b,2))
+        b_idx = re.index('b')
+        return re[b_idx+1:]
