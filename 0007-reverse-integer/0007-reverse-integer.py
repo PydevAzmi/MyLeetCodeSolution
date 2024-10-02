6ba6622c54ce8a1f91502607f69b1aac6a88c735
Mohamed Azmii <pydevazmi@gmail.com>
2024-10-02 21:31:51 +0300

    Time: 19 ms (32.02%), Space: 11.4 MB (93.28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,10 +4,11 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if abs(x) < 2 ** 31 - 1:
-            if x>=0:
-                result = int("".join([s for s in str(x)[::-1]]))
-            else:
-                result = int("".join([s for s in str(x)[:0:-1]]))*-1
-            return result if abs(result) <= 2 ** 31 - 1 else 0
-        return 0
+        if abs(x) > 2 ** 31 - 1:
+            return 0
+        
+        if x>=0:
+            result = int("".join([s for s in str(x)[::-1]]))
+        else:
+            result = int("".join([s for s in str(x)[:0:-1]]))*-1
+        return result if abs(result) <= 2 ** 31 - 1 else 0
